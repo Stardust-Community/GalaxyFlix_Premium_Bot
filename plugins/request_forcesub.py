2a56043b63ec847a025711e0fbd9ccb7fa8490de
SID2 <shidoteshika2@gmail.com>
2024-09-26 15:14:25 +0530

    Update request_forcesub.py

--- plugins/request_forcesub.py
+++ plugins/request_forcesub.py
@@ -1,1 +1,13 @@
 #adding soon
+import logging
+import os
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors import FloodWait
+
+from bot import Bot
+from config import FORCE_MSG, START_MSG, CUSTOM_CAPTION, LOG_CHNL, OWNER_ID
+from helper_func import is_userJoin, is_admin, subscribed, encode, decode, get_messages
+from database.database import kingdb#get_all_channels, store_req_link, get_req_link, del_req_link, store_reqsent_id, get_reqsent_ids, del_reqsent_id, get_request_forcesub, set_request_forcesub, get_ban_users
