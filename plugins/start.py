7626b630b4640c94a949753d3e2eef6ea2db87bb
SID2 <shidoteshika2@gmail.com>
2024-09-27 08:30:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -166,7 +166,7 @@ async def not_joined(client: Client, message: Message):
 
     try:
         for id in channels:
-            await message.reply_chat_action(ChatAction.RECORD_AUDIO)
+            await message.reply_chat_action(ChatAction.FIND_LOCATION)
             if not await is_userJoin(client, user_id, id):
                 try:
                     data = await client.get_chat(id)
