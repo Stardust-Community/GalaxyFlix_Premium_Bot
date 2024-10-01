7551128eee305333237132bc9add2c393327f41b
Shido <shidoteshika2@gmail.com>
2024-10-01 22:44:56 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from helper_func import encode, get_message_id, is_admin
 
-@Bot.on_message(filters.private & filters.command('batch') & is_admin)
+@Bot.on_message(is_admin & filters.private & filters.command('batch'))
 async def batch(client: Client, message: Message):    
     while True:
         try:
@@ -40,7 +40,7 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Bᴇʟᴏᴡ ɪs ʏᴏᴜʀ ʟɪɴᴋ:</b>\n<blockquote>{link}</blockquote>", quote=True, reply_markup=reply_markup, disable_web_page_preview=True)
 
 
-@Bot.on_message(filters.private & filters.command('genlink') & is_admin)
+@Bot.on_message(is_admin & filters.private & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
