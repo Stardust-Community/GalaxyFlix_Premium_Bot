4c169365662358d78e02a793adb19ceaf9fa5e6a
Stardust-Community <arkasarkar235@gmail.com>
2024-12-14 16:06:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -115,7 +115,7 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-	        message_effect_id=5044134455711629726 #❤️
+	        message_effect_id=5104841245755180586 #🔥
         )
         try: await message.delete()
         except: pass
