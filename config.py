7a648eaac63aa5edd9bccfcdedb17df19b0c72c7
Stardust-Community <arkasarkar235@gmail.com>
2024-12-25 23:34:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster5454")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/J3r.jpg https://envs.sh/J3s.jpg https://envs.sh/J39.jpg https://envs.sh/J3v.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "")).split() #Required
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
